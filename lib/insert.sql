a3ba2a5bc124594448b8bfe29407181a4db8db6a
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-06-02 16:19:23 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+-- INSERT INTO bears table the column details and there coresponding Values
+-- SQL boolean are stored as Integers 0= False, 1= True
+
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL,             1,"F",   "White",      "Tender",        0)
+      ("Black_Bear",    20,"M",   "Black",       "Aggresive",    1),
+      ("Sloth_Bear",    14, "M",  "Gray",        "Playful",      1),
+      ("Giant_Pand",    30, "F",  "White_Black", "Calm",         0),
+      ("Procyonid ",    13, "M",  "Brown",       "Cheerful",     1),
+      ("Bear_Baiting",  12, "F",  "Brown",       "Restless",     1),
+      ("Spectacled_Bear",7, "M",  "Dark_Brown",  "Aggresive",    0),
+      ("Gizzly_Bear",    4, "F",  "Brown",       "Tender",       1),
+      (NULL,             3, "M",  "White",       "Violent",      1);
